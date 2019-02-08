914603fe16c66d3e6d0d2520d544f28eb82edada
Chaithanya Cherukuru <S531495@nwmissouro.edu>
2019-02-08 13:57:43 -0600

    "src"

---
+++ src/pg/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace pg.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
